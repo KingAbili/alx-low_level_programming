7d1486fe88f8e69a1db7b60c381b25874d961c11
KingAbili <davidabili3@gmail.com>
2023-06-15 12:21:27 -0700

    printing various sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * A program that prints the size  of various types
+* return: 0 (success)
+ */
+
+int main(void)
+{
+	printf("Size of a char: 1 byte(s)\n
+Size of an int: 4 byte(s)\n
+Size of a long int: 4 byte(s)\n
+Size of a long long int: 8 byte(s)\n
+Size of a float: 4 byte(s)\n")
+		return (0);
+}
