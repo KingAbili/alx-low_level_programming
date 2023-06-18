1f14209c8c32d9b3011cf0222f30f3842c1dd811
KingAbili <davidabili3@gmail.com>
2023-06-18 06:41:55 -0700

    printing multiple single digits number

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * a program to print combinations of a single number
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int king;
+
+	king = 0;
+	while (king < 10)
+	{
+		putchar('0' + king);
+		if (king < 9)
+		{
+			putchar(' ');
+			putchar(',');
+		}
+		king++;
+	}
+	putchar('\n');
+	return (0);
+}
