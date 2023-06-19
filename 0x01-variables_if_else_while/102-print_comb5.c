9d688e6b79272ca793210cd90cc96da23b788738
KingAbili <davidabili3@gmail.com>
2023-06-19 11:35:54 -0700

    doing hard things, alx no be beans

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * a program that prints combinations, this program burst my head
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int a, b, c, d;
+
+	a = 0;
+	while (a < 10)
+	{
+		b = 0;
+		while (b < 10)
+		{
+			c = a;
+			while (c < 10)
+			{
+				d = b + 1;
+				while (d < 10)
+				{
+					putchar('0' + a);
+					putchar('0' + b);
+					putchar(' ');
+					putchar('0' + c);
+					putchar('0' + d);
+					if (a != 9 || b != 8)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					d++;
+				}
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
