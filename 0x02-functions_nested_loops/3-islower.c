7fb8fd5afc7feaef939444846bdc46cc62e55784
KingAbili <davidabili3@gmail.com>
2023-06-21 07:11:57 -0700

    lower alphabets

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - entry point of function
+ * @c: Character to be considered
+ * Return: 1 for lowercase or 0 for others
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (0);
+	}
+	return (1);
+}
