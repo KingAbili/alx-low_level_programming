aad3ff4cca819f4b7d8ca990e5c0080fdbfb7753
KingAbili <davidabili3@gmail.com>
2023-06-21 15:25:52 -0700

    adding numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - entry point
+ * a program to add two new numbers
+ * @a: first number
+ * @b: second number
+ * Return: integer values successful
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
