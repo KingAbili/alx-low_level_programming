5b70b21300683a98b0e23b4676949acf26955336
KingAbili <davidabili3@gmail.com>
2023-06-23 06:03:56 -0700

    numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - entry point
+ * a program to print numbers
+ * Return: void value
+ */
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0' + num);
+	}
+	_putchar('\n');
+}
