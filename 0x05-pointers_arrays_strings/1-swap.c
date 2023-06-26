d1ace11677e24df2c7dc9e3760b49ffe14b62c37
KingAbili <davidabili3@gmail.com>
2023-06-26 15:05:11 -0700

    swapping the values of integer

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - entry
+ * @a - variable to be considered
+ * @b - variable to be considered
+ * Return: void (succesful)
+ */
+
+void swap_int(int *a, int *b)
+{
+	int num;
+
+	num = *a;
+	*a = *b;
+	*b = num;
+}
