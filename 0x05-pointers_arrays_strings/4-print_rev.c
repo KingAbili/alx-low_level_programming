5ecfba1b32097b0f03f5b8d73409d6bbe580403e
KingAbili <davidabili3@gmail.com>
2023-06-27 13:52:55 -0700

    printing in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - entry point
+ * @s: the string
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int a = 0;
+
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+	for (a -= 1; a >= 0; a--)
+	{
+		_putchar(s[a]);
+	}
+	_putchar('\n');
+}
