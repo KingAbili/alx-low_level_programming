1870aaad2c6de0fc3c275e709eda1dc57e45ef03
KingAbili <davidabili3@gmail.com>
2023-07-03 14:02:28 -0700

    seting a value of a pointer to a char

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - Entry
+ * @s: character to consider
+ * @to: charater to check
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
