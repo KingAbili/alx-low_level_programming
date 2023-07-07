8409c928e2d79898c9becc59287968d9ea8297ad
KingAbili <davidabili3@gmail.com>
2023-07-07 14:42:25 -0700

    number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry, number of args
+ * @argc: argument
+ * @argv: vector
+ * Return: 0 successful
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d", argc - 1);
+	return (0);
+}
