c54b3804a7d1c09a4f8e24ff7942e1853ad70cde
KingAbili <davidabili3@gmail.com>
2023-07-07 14:35:38 -0700

    my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry, what's my name
+ * @argc: count
+ * @argv: vector
+ * Return: 0 successful
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
